3a30b0e57c5f6b0b5187aa2772da7f6cca7b83a9
humam sab <mohammedhumams0100@gmail.com>
2025-09-07 18:10:58 +0530

    Initial commit - React Offline project with Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-green-900 text-black p-4 rounded-xl justify-center">Tailwind is Working!</h1>
+    </div>
+  );
+}
+
+export default App;
